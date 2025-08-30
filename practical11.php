d03dfa6c2a6e9efde9e071a6b186864190de7191
pooja200333 <poojayadav502525@gmail.com>
2025-08-30 15:54:39 +0530

    Add files via upload

---
+++ practical11.php
@@ -0,0 +1,15 @@
+<?php
+  $num1=5;
+  $num2=10;
+  $sum=$num1+$num2;
+    echo "num1= $num1";
+    echo "<br>";
+     echo "num2= $num2";
+      echo "<br>";
+     echo "Sum of $num1 + $num2=$sum";
+
+
+    
+
+
+?>
